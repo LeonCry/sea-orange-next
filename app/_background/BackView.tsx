171195a82dd8c4f1f44a4c1ebee5e9ee03b609ec
343347@libinghai <1395346178@qq.com>
2024-03-01 14:54:13 +0800

    fix: app mutil render

--- app/_background/BackView.tsx
+++ app/_background/BackView.tsx
@@ -2,10 +2,16 @@
 import { Stage, AppProvider } from '@pixi/react';
 import { Application } from 'pixi.js';
 import TreeGrow from './TreeGrow';
+import { useEffect, useMemo } from 'react';
 const BackView = () => {
   const width = document.documentElement.clientWidth;
   const height = document.documentElement.clientHeight;
-  const app = new Application({ width, height });
+  const app = useMemo(() => new Application({ width, height }), []);
+  useEffect(() => {
+    return () => {
+      app.destroy(true);
+    };
+  }, [app]);
   return (
     <main className=" absolute top-0 w-full h-full bg-base-bg-color -z-10 blur-[1px]">
       <AppProvider value={app}>
