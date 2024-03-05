4f90fbe0841d73db0e81ed92aba7a034d9db1783
343347@libinghai <1395346178@qq.com>
2024-03-05 09:58:21 +0800

    fix: build error

--- api/getSectionInfo.ts
+++ api/getSectionInfo.ts
@@ -4,10 +4,10 @@ import executeWithDb from "./executeWithDb";
 import prisma from "@/lib/prisma";
 //获得每个section的header信息
 const getSectionInfo = async () => {
-  return executeWithDb(() => prisma.SectionInfo.findMany());
+  return executeWithDb(() => prisma.sectionInfo.findMany());
 };
 //提交每个界面的访问信息
 const uploadVisit = async (info: Omit<VisitTime, 'id'>) => {
-  return executeWithDb(() => prisma.VisitTime.create({ data: info }));
+  return executeWithDb(() => prisma.visitTime.create({ data: info }));
 };
 export { getSectionInfo, uploadVisit };
