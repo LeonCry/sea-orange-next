0d39710c209f29340e6056e92f6ad629ccedc006
343347@libinghai <1395346178@qq.com>
2024-04-28 11:40:34 +0800

    refactor: refactore

--- app/funny/_items/democracySimulator/component/OpButton.tsx
+++ app/funny/_items/democracySimulator/component/OpButton.tsx
@@ -1,11 +1,16 @@
-const OpButton = ({ title }: { title: string }) => {
-  return (
-    <>
-      <button className=" flex-1 cursor-none border-2 border-zinc-600 p-2 text-yellow-400 hover:bg-yellow-500 hover:text-zinc-800 hover:border-gray-200">
-        {title}
-      </button>
-    </>
-  );
-};
+import { memo } from 'react';
 
+const OpButton = memo(
+  ({ title }: { title: string }) => {
+    return (
+      <>
+        <button className=" flex-1 cursor-none border-2 border-zinc-600 p-2 text-yellow-400 hover:bg-yellow-500 hover:text-zinc-800 hover:border-gray-200">
+          {title}
+        </button>
+      </>
+    );
+  },
+  (p, n) => p.title !== n.title
+);
+OpButton.displayName = 'OpButton';
 export default OpButton;
