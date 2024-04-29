ff35418f4a82329e626ecb6aec8cf13e1569dee3
343347@libinghai <1395346178@qq.com>
2024-04-29 19:51:51 +0800

    feat: update hooks

--- app/funny/_items/demo/index.tsx
+++ app/funny/_items/demo/index.tsx
@@ -1,6 +1,33 @@
 'use client';
-const index = () => {
-  return <div>THIS IS A DEMO.</div>;
+import usePreloadImages from '@/hooks/usePreloadImages';
+import { useRef, useState } from 'react';
+const Index = () => {
+  const [imageUrl, setImageUrl] = useState('https://s2.loli.net/2024/04/29/l1EgJNeL2qxCdAv.jpg');
+  const allImages = useRef([
+    'https://s2.loli.net/2024/04/29/2wWthGerapL6nYN.jpg',
+    'https://s2.loli.net/2024/04/29/iJER1aKzymgtcBU.jpg',
+    'https://s2.loli.net/2024/04/29/zkPYOCxmEJsyT1p.jpg',
+    'https://s2.loli.net/2024/04/29/l1EgJNeL2qxCdAv.jpg',
+  ]);
+  const changeImageUrl = () => {
+    const image = allImages.current.shift();
+    if (!image) return;
+    setImageUrl(image);
+  };
+  const { process, total } = usePreloadImages(allImages.current);
+  return (
+    <section>
+      <button className=" absolute right-20 z-10" onClick={changeImageUrl}>
+        change
+      </button>
+      <picture>
+        <img src={imageUrl} alt="pic" />
+      </picture>
+      <span>
+        loading: {process} / {total}
+      </span>
+    </section>
+  );
 };
 
-export default index;
+export default Index;
---
+++ hooks/usePreloadImages.ts
@@ -0,0 +1,19 @@
+import { useGetState, useMemoizedFn } from "ahooks";
+import { throttle } from "radash";
+import { useEffect, useMemo, useRef } from "react";
+const usePreloadImages = (images: string[]) => {
+  const getImages = useMemoizedFn(throttle({ interval: 100 }, () => images.forEach((mg) => {
+    const imgElement = new Image();
+    imgElement.src = mg;
+    imgElement.onload = () => {
+      setProcess((p) => ++p);
+    };
+  })));
+  const [, setProcess, getProcess] = useGetState(0);
+  const total = useMemo(() => images.length, []);
+  useEffect(() => {
+    getImages();
+  }, []);
+  return { process: getProcess(), total };
+};
+export default usePreloadImages;
