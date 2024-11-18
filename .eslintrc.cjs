3d95a67e0f1dca6cd876af91cc26e62fef1edaf1
343347@libinghai <1395346178@qq.com>
2024-11-18 09:55:25 +0800

    ci: :ferris_wheel: update eslint package

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'next/core-web-vitals',
+  rules: {
+    semi: ['warn', 'always'],
+    quotes: ['warn', 'single'],
+    eqeqeq: ['error', 'always'],
+    'no-console': 'off',
+  },
+};
