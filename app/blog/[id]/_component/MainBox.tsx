99a9186834921b5369b7be7772124970d6f36d62
343347@libinghai <1395346178@qq.com>
2024-12-13 16:22:42 +0800

    feat: :sparkles: blog更新

--- app/blog/[id]/_component/MainBox.tsx
+++ app/blog/[id]/_component/MainBox.tsx
@@ -1,13 +1,22 @@
 import { getBlogInfoById } from '@/api/blogPageApi';
 import style from './MainBox.module.scss';
 import Md from './Md';
+import Link from 'next/link';
 import { BlogPageItem } from '@prisma/client';
 const MainBox = async ({ mdId }: { mdId: string }) => {
   const blogInfo: BlogPageItem = await getBlogInfoById(parseInt(mdId));
   return (
-    <section className={`${style.main} drop-animation`}>
-      <Md blogInfo={blogInfo} />
-    </section>
+    <>
+      <Link
+        className={`${style.exit} absolute top-20 left-8 cursor-none transition-all duration-500 px-2 py-1 rounded box-trigger`}
+        href={'/blog'}
+      >
+        EXIT
+      </Link>
+      <section className={`${style.main} drop-animation`}>
+        <Md blogInfo={blogInfo} />
+      </section>
+    </>
   );
 };
 
--- app/blog/[id]/_component/Md.tsx
+++ app/blog/[id]/_component/Md.tsx
@@ -1,4 +1,3 @@
-'use client';
 import { BlogPageItem } from '@prisma/client';
 import './prose-md.scss';
 import style from './MainBox.module.scss';
@@ -6,18 +5,10 @@ import rehypeRaw from 'rehype-raw';
 import gfm from 'remark-gfm';
 import ReactMarkdown from 'react-markdown';
 import { CodeBlock } from './CodeBlock';
-import Link from 'next/link';
-import { LinkLeft } from '@icon-park/react';
 const Md = ({ blogInfo }: { blogInfo: BlogPageItem }) => {
   const { content } = blogInfo;
   return (
-    <section className=" overflow-auto">
-      <Link
-        className=" absolute top-0 left-0 cursor-none transition-all duration-300 hover:rotate-[375deg]"
-        href={'/blog'}
-      >
-        <LinkLeft theme="outline" size="20" fill="#000000" />
-      </Link>
+    <section className="overflow-auto">
       <article className={style.md}>
         <ReactMarkdown
           className=" prose prose-pre:bg-white prose-pre:overflow-auto prose-pre:p-0 !max-w-full"
