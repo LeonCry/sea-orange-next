fb1d6a7a071191f3b2fcb9035f03256ca277289f
343347@libinghai <1395346178@qq.com>
2024-12-24 15:24:04 +0800

    feat: :sparkles: camera unstable_cache

--- app/camera/[id]/page.tsx
+++ app/camera/[id]/page.tsx
@@ -1,9 +1,12 @@
+import { unstable_cache } from 'next/cache';
 import CameraInfo from './CameraInfo';
 import { getPhotoById } from '@/api/cameraPageApi';
 
 const CameraPage = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
-  const photoInfo = await getPhotoById(parseInt(id));
+  const photoInfo = await unstable_cache(async () => await getPhotoById(parseInt(id)), [id], {
+    tags: ['/camera' + id],
+  })();
   return <CameraInfo local={photoInfo} />;
 };
 export default CameraPage;
