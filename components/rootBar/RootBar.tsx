84a4b2b92ecf4166fdcf7784e145d44770fe4d55
343347@libinghai <1395346178@qq.com>
2024-12-25 11:35:12 +0800

    feat: :sparkles: rootbar 更改样式

--- components/rootBar/RootBar.tsx
+++ components/rootBar/RootBar.tsx
@@ -4,9 +4,17 @@ import '@/app/enter-fade.scss';
 import { AppProgressBar } from 'next-nprogress-bar';
 import { chakraEN } from '@/style/defineFont';
 import { usePathname } from 'next/navigation';
-import { Like, MessageEmoji, Tv, GithubOne, CastScreen, DarkMode } from '@icon-park/react';
+import {
+  Like,
+  MessageEmoji,
+  Tv,
+  GithubOne,
+  CastScreen,
+  DarkMode,
+  UserPositioning,
+} from '@icon-park/react';
 import style from './RootBar.module.scss';
-import { useEffect, useState, useMemo, useCallback } from 'react';
+import { useEffect, useState, useMemo, useCallback, useRef } from 'react';
 import { alertInfo, tvUrl, gitUrl } from './Info';
 import { darkStore } from '@/store/darkStore';
 import { useEffectOnce } from 'react-use';
@@ -15,7 +23,6 @@ import NumberFlow from '@number-flow/react';
 const RootBar = () => {
   const pathName = usePathname();
   const secPath = '/' + pathName.split('/')[1];
-  const liActiveStyle = 'border-b-[1px] border-black text-[#181926]';
   const [isLike, isLikeSet] = useState(false);
   const [isDark, isDarkSet] = useState(false);
 
@@ -66,16 +73,23 @@ const RootBar = () => {
     }
   }, []);
   const [visitCount, setVisitCount] = useState(0);
+  const [isCount, setIsCount] = useState(false);
+  const timer = useRef<NodeJS.Timeout | null>(null);
   const getVisitCount = useCallback(async () => {
     const count = await getCount();
+    clearInterval(timer.current!);
+    setIsCount(true);
     setVisitCount(count);
     await new Promise((resolve) => setTimeout(resolve, 2000));
     setVisitCount(count + 1);
   }, []);
   useEffectOnce(() => {
     getVisitCount();
+    timer.current = setInterval(() => {
+      setVisitCount(Math.floor(100 + Math.random() * 900));
+    }, 1000);
   });
-
+  const activeStyle = '!text-[#585b70] bg-[#ffffffea] shadow rounded-xl animate-play';
   return (
     <>
       <AppProgressBar height="2px" color="#ff9103" options={{ showSpinner: false }} />
@@ -83,71 +97,88 @@ const RootBar = () => {
         <ol
           className={`${style.main} w-full h-16 flex items-center enterFade justify-end ${chakraEN.className}`}
         >
-          <div className=" text-gray-300 px-2 flex gap-1 items-center text-xs absolute w-fit text-center top-2 left-2">
-            <NumberFlow
-              prefix="0"
-              value={visitCount}
-              transformTiming={{ duration: 1000, easing: 'ease-out' }}
-              continuous
-              trend={1}
-            />
-            {visitCount && (
-              <span className={`text-gray-300 text-xs block opacity-0 ${style.visAdd}`}> +1 </span>
-            )}
+          <div className="h-12 flex items-center enterFade w-fit px-2 mt-4 bg-[#eaeaeb99] rounded-2xl">
+            <li className={`${style.rootBar}  ${secPath === '/blog' && activeStyle}`}>
+              <Link className="cursor-none" href={'/blog'}>
+                BLOG
+              </Link>
+            </li>
+            <li className={`${style.rootBar} ${secPath === '/project' && activeStyle}`}>
+              <Link className="cursor-none" href={'/project'}>
+                PROJECT
+              </Link>
+            </li>
+            <li className={`${style.rootBar} ${secPath === '/camera' && activeStyle}`}>
+              <Link className="cursor-none" href={'/camera'}>
+                CAMERA
+              </Link>
+            </li>
+            <li className={`${style.rootBar} ${secPath === '/funny' && activeStyle}`}>
+              <Link className="cursor-none" href={'/funny'}>
+                FUNNY
+              </Link>
+            </li>
+            <li className={`${style.rootBar} ${secPath === '/gossip' && activeStyle}`}>
+              <Link className="cursor-none" href={'/gossip'}>
+                GOSSIP
+              </Link>
+            </li>
+            <li className={`${style.rootBar} ${secPath === '/about' && activeStyle}`}>
+              <Link className="cursor-none" href={'/about'}>
+                ABOUT
+              </Link>
+            </li>
           </div>
-          <li className={`${style.rootBar}  ${secPath === '/blog' && liActiveStyle}`}>
-            <Link className="cursor-none" href={'/blog'}>
-              BLOG
-            </Link>
-          </li>
-          <li className={`${style.rootBar} ${secPath === '/project' && liActiveStyle}`}>
-            <Link className="cursor-none" href={'/project'}>
-              PROJECT
-            </Link>
-          </li>
-          <li className={`${style.rootBar} ${secPath === '/camera' && liActiveStyle}`}>
-            <Link className="cursor-none" href={'/camera'}>
-              CAMERA
-            </Link>
-          </li>
-          <li className={`${style.rootBar} ${secPath === '/funny' && liActiveStyle}`}>
-            <Link className="cursor-none" href={'/funny'}>
-              FUNNY
-            </Link>
-          </li>
-          <li className={`${style.rootBar} ${secPath === '/gossip' && liActiveStyle}`}>
-            <Link className="cursor-none" href={'/gossip'}>
-              GOSSIP
-            </Link>
-          </li>
-          <li className={`${style.rootBar} ${secPath === '/about' && liActiveStyle}`}>
-            <Link className="cursor-none" href={'/about'}>
-              ABOUT
-            </Link>
-          </li>
-          <li className="w-16 text-center select-none"> || </li>
-          <li onClick={handleLike} className={style.icon}>
-            {memoizedLike}
-          </li>
-          <li className={style.icon} onClick={() => window.alert(alertInfo)}>
-            <MessageEmoji theme="outline" size="20" fill="#181926" />
-          </li>
-          <li className={style.icon}>
-            <a className="cursor-none" href={tvUrl} target="_blank">
-              <Tv theme="outline" size="20" fill="#181926" />
-            </a>
-          </li>
-          <li className={style.icon}>
-            <a className="cursor-none" href={gitUrl} target="_blank">
-              <GithubOne theme="outline" size="20" fill="#181926" />
-            </a>
-          </li>
-          <li className={style.icon}>
-            <CastScreen theme="outline" size="20" fill="#181926" />
-          </li>
-          <li className={`${style.icon} mr-10`} onClick={handleDark}>
-            {memoizedDarkMode}
-          </li>
+          <li className="w-6 text-center h-3 select-none bg-[#eaeaeb99]" />
+          <div className="h-12 flex items-center enterFade w-fit px-2 mt-4 bg-[#eaeaeb99] rounded-2xl">
+            <li onClick={handleLike} className={style.icon}>
+              {memoizedLike}
+            </li>
+            <li className={style.icon} onClick={() => window.alert(alertInfo)}>
+              <MessageEmoji theme="outline" size="20" fill="#181926" />
+            </li>
+            <li className={style.icon}>
+              <a className="cursor-none" href={tvUrl} target="_blank">
+                <Tv theme="outline" size="20" fill="#181926" />
+              </a>
+            </li>
+            <li className={style.icon}>
+              <a className="cursor-none" href={gitUrl} target="_blank">
+                <GithubOne theme="outline" size="20" fill="#181926" />
+              </a>
+            </li>
+            <li className={style.icon}>
+              <CastScreen theme="outline" size="20" fill="#181926" />
+            </li>
+            <li className={`${style.icon} mr-10`} onClick={handleDark}>
+              {memoizedDarkMode}
+            </li>
+          </div>
+          <li className="w-6 text-center h-3 select-none bg-[#eaeaeb99]" />
+          <div className="h-12 mr-4 flex items-center enterFade w-fit mt-4 bg-[#eaeaeb99] rounded-2xl transition-all hover:bg-[#8686ff31]">
+            <li className="px-2 py-1 mx-1 text-[#585b70a7]">
+              <Link className="cursor-none flex items-center gap-1" href={'/user'}>
+                <UserPositioning theme="outline" size="20" fill="#585b70d2" />
+                <div className=" text-gray-400 relative flex gap-1 items-center text-xs w-fit text-center">
+                  <NumberFlow
+                    prefix="0"
+                    value={visitCount}
+                    transformTiming={{ duration: 1000, easing: 'linear' }}
+                    continuous
+                    trend={1}
+                  />
+                  {isCount && (
+                    <span
+                      className={`text-gray-300 absolute -right-4 text-xs block opacity-0 ${style.visAdd}`}
+                    >
+                      {' '}
+                      +1{' '}
+                    </span>
+                  )}
+                </div>
+              </Link>
+            </li>
+          </div>
         </ol>
       </header>
     </>
