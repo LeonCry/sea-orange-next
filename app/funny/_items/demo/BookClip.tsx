3666e999addd8530ef3cce8193417cfdad026ff1
343347@libinghai <1395346178@qq.com>
2025-01-08 19:51:24 +0800

    feat: :sparkles: add 动画

--- app/funny/_items/demo/BookClip.tsx
+++ app/funny/_items/demo/BookClip.tsx
@@ -1,13 +1,19 @@
 import clsx from 'clsx';
 import style from './index.module.scss';
-const BookClip = () => {
+const BookClip = ({ index, bookInfo }: { index: number; bookInfo: any }) => {
   return (
-    <aside className=" absolute left-[15%] w-[70%] h-full z-10">
+    <aside
+      className={clsx(['absolute w-[70%] left-[12%] h-full z-10', style[`clipAni${index}`]])}
+      style={{ left: `${12 + index * 3}%`, top: `${40 - index * 12}%`, zIndex: 100 - index }}
+    >
       <div className="flex w-full h-[7%]">
-        <div className={clsx([style.bookTop, 'bg-red-400'])} />
-        <div className={clsx([style.clip, 'w-[200px] h-full bg-red-400'])} />
+        <div className={style.bookTop} style={{ backgroundColor: bookInfo.color }} />
+        <div className={style.clip} style={{ backgroundColor: bookInfo.color }} />
       </div>
-      <div className="w-full h-[92%] bg-red-400 rounded-b-2xl rounded-tr-[2rem]"></div>
+      <div
+        className="w-full h-[92%] rounded-b-2xl rounded-tr-[2rem]"
+        style={{ backgroundColor: bookInfo.color }}
+      ></div>
     </aside>
   );
 };
--- app/funny/_items/demo/index.tsx
+++ app/funny/_items/demo/index.tsx
@@ -1,13 +1,40 @@
 'use client';
-
 import BookBox from './BookBox';
 import BookClip from './BookClip';
-
+import style from './index.module.scss';
 const Index = () => {
+  const books = [
+    {
+      id: 1,
+      title: 'Book 1',
+      author: 'Author 1',
+      color: 'red',
+    },
+    {
+      id: 2,
+      title: 'Book 2',
+      author: 'Author 2',
+      color: 'blue',
+    },
+    {
+      id: 3,
+      title: 'Book 3',
+      author: 'Author 3',
+      color: 'green',
+    },
+    {
+      id: 4,
+      title: 'Book 4',
+      author: 'Author 4',
+      color: 'yellow',
+    },
+  ];
   return (
     <section className="w-full h-full overflow-hidden backdrop-blur">
-      <BookClip />
-      <BookBox />
+      {books.map((book, i) => (
+        <BookClip key={book.id} index={i} bookInfo={book} />
+      ))}
+      <BookBox innerClass={style.boxAnimation} />
     </section>
   );
 };
