88746a732f990717bbde4696e581ab90b27ad6af
voidis <lbh_ldu@outlook.com>
2025-10-29 16:11:15 +0800

    refactor(about): 移除未使用的语言参数

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,8 +2,7 @@ import '../globals.css';
 import AboutMe from './AboutMe';
 import RandomSpan from '@/components/randomSpan/RandomSpan';
 
-const About = ({ searchParams }: { searchParams?: { lang?: string } }) => {
-  const lang = searchParams?.lang === 'zh' ? 'zh' : 'en'; 
+const About = () => {
   return (
     <section className="page-dropDown fix-h">
       <AboutMe />
